a6de7653fb750f839d9dd023ceb88b1f24b72b9e
Deepu Premrajan <deepu.premrajan@gmail.com>
2023-08-15 19:23:18 -0700

    fixed handleImage function of onboarding

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -45,12 +45,27 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
   });
 
   const handleImage = (
-    e: ChangeEvent,
+    e: ChangeEvent<HTMLInputElement>,
     fieldChange: (value: string) => void
   ) => {
     e.preventDefault();
 
     const fileReader = new FileReader();
+
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+
+      setFiles(Array.from(e.target.files));
+
+      if (!file.type.includes("image")) return;
+
+      fileReader.onload = async (event) => {
+        const imageDataUrl = event.target?.result?.toString() || "";
+        fieldChange(imageDataUrl);
+      };
+
+      fileReader.readAsDataURL(file);
+    }
   };
 
   function onSubmit(values: z.infer<typeof UserValidation>) {
